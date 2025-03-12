825a19644cf98a9b704460e7901bb27adf642d09
Domisnnet <dominique.goncalves61@gmail.com>
2025-03-12 20:31:01 -0300

    alterado

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,15 +1,16 @@
 import './Nav.css'
 import React from 'react'
 
-export default() => (
+export default props => 
     <aside className="menu-area">
         <nav className="menu">
-            <a href="#/">
-                <i className="fa fa-home"></i> Início
+            <a href="#/Home">
+                <i className="fa fa-home">
+                </i> Início
             </a>
-            <a href="#/">
-                <i className="fa fa-users"></i> Usuários
+            <a href="#/Usuários">
+                <i className="fa fa-users">
+                </i> Usuários
             </a>
         </nav>
     </aside>
-);
